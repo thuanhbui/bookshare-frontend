49944cb969e6ee0e5f2d62d997f5115b359c639e
thuanhbui <buithulop5ccx@gmail.com>
2021-11-01 11:01:37 +0700

    feat/homepage

---
+++ src/components/list-item/index.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import style from "./list-item.module.scss";
+import { useRouter } from "next/router";
+import { SeeMoreNoResult } from "../no-result";
+import { PageNavigation } from "../pagination";
+import { ItemComponent } from "../item";
+
+export const ListItems = ({ selectedCate }) => {
+  const router = useRouter();
+
+  const [totalProduct, setTotalProduct] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [itemsPerPage, setItemsPerPage] = useState(20);
+  const [page, setPage] = useState(1);
+  const [dataListProducts, setDataListProducts] = useState(null);
+  const [category, setCategory] = useState(router.query["category"]);
+
+  useEffect(() => {
+    router.isReady && setCategory(router.query.category);
+  }, [router]);
+
+  useEffect(() => {
+    featDataListProducts(selectedCate);
+  }, [category, page, selectedCate, itemsPerPage]);
+
+  const featDataListProducts = (selectedCate) => {
+    setIsLoading(true);
+    
+    setDataListProducts(Array(20).fill(0).map((i, index) => ({id: index})));
+    setTotalProduct(dataListProducts?.length);
+    setIsLoading(false);
+
+    // SeriesManagementAPI.getSerieQuery({
+    //   userInfo: GetUserInfo(),
+    //   limit: itemsPerPage,
+    //   page: page,
+    //   category: selectedCate,
+    //   isDaily: "true",
+    // })
+    //   .then((res) => {
+    //     console.log({ res })
+    //     setDataListProducts(res.data);
+    //     setTotalProduct(res.totalSeries);
+    //     setIsLoading(false);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     setTotalProduct(0);
+    //     setIsLoading(false);
+    //   });
+  };
+
+  return (
+    <div className={style["list-series-container"]} id="main-container">
+      <div className={`${style["list-series-tag"]}`}>New Released</div>
+      {!isLoading && totalProduct === 0 ? (
+        <SeeMoreNoResult />
+      ) : (
+        <>
+          <div className={`${style["list-series-content"]}`}>
+            {!isLoading &&
+              dataListProducts?.map((serie, index) => (
+                <ItemComponent key={index} id={serie.id} />
+              ))}
+          </div>
+          {!isLoading && totalProduct > itemsPerPage && (
+            <PageNavigation
+              page={page}
+              setPage={setPage}
+              totalItem={totalProduct}
+              itemsPerPage={itemsPerPage}
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import { Header } from '../components/header'
 import { ItemComponent } from '../components/item'
 import { HomePage } from '../templates/homepage'
+import React, { useEffect, useState, useRef } from "react";
+import { SubHeader } from '../components/sub-header';
+import { ListItems } from '../components/list-item';
+
 
 const Home: React.FC<{ homepageContent: any }> = () => {
+
+  const [selectedCate, setSelectedCate] = useState("all");
+
   return (
     <div>
       <Header/>
+      <SubHeader selectedCate={selectedCate}
+                            setSelectedCate={setSelectedCate} />
       <div style={{height: 50}}></div>
-      <ItemComponent/>
+      <ListItems selectedCate={selectedCate}/>
     </div>
   )
 }
