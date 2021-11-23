0669f77b580c8f96c3e15e490c42019f90d136fc
thuanhbui <buithulop5ccx@gmail.com>
2021-11-23 21:39:09 +0700

    feat:signup

--- src/api/user.js
+++ src/api/user.js
@@ -34,4 +34,36 @@ export default {
       return data;
     });
   },
+  checkUsernameAvailable: ({ username }) => {
+    const _axios = axios.create();
+    return _axios({
+      method: "get",
+      url: `${API_BASE_URL}/users?username=${username}`,
+    }).then((data) => {
+      return true;
+    });
+  },
+  checkEmailExist: ({ email }) => {
+    const _axios = axios.create();
+    return _axios({
+      method: "get",
+      url: `${API_BASE_URL}/catalogs/all`,
+    })
+      .then((data) => {
+        return true;
+      })
+      .catch((err) => {
+        return true;
+      });
+  },
+  createNewUser: ({ userInfo }) => {
+    const _axios = axios.create();
+    return _axios({
+      method: "post",
+      url: `${API_BASE_URL}/users`,
+      data: userInfo,
+    }).then((data) => {
+      return data;
+    });
+  },
 };
